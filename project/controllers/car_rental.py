51167f527de606acb0b1b406755d454bca9fd283
Lucas Grandal Lama <113071328+lucasgrandal@users.noreply.github.com>
2023-11-03 20:52:59 +0100

    Update car_rental.py

--- project/controllers/car_rental.py
+++ project/controllers/car_rental.py
@@ -1,5 +1,5 @@
 from project import app
-from flask import render_template, request, redirect, url_for 
+from flask import request
 from project.models.car import *
 from project.models.costumer import *
 from project.models.employee import *
@@ -8,42 +8,42 @@ from project.models.employee import *
 def order_car(id, reg):
     costumer = read_costumer(id)
     car = read_car(reg)
-    if(costumer[4] == "null" & car[5] == "available"):
-        car[5]="booked"
+    if(costumer['booked'] == "null" and car['status'] == "available"):
+        car['status']="booked"
 
-    return update_car(car[0], car[1], car[2], car[3], car[4], car[5])
+    return update_car(car['make'], car['model'], car['year'], car['reg'], car['location'], car['status'])
 
-@app.route('/cancel-order-car', methods=['GET']) 
+@app.route('/cancel-order-car', methods=['POST']) 
 def cancel_order_car(id, reg):
     costumer = read_costumer(id)
     car = read_car(reg)
-    if(costumer[4] == car[2] & car[5] == "booked"):
-        car[5]="available"
-        update_costumer(costumer[0], costumer[1], costumer[2], costumer[3], "null")
+    if(costumer['booked'] == car['reg'] and car['status'] == "booked"):
+        car['status']="available"
+        update_costumer(costumer['name'], costumer['id'], costumer['age'], costumer['address'], "null")
 
-    return update_car(car[0], car[1], car[2], car[3], car[4], car[5])
+    return update_car(car['make'], car['model'], car['year'], car['reg'], car['location'], car['status'])
 
 @app.route('/rent-car', methods=['GET']) 
 def rent_car(id, reg):
     costumer = read_costumer(id)
     car = read_car(reg)
-    if(costumer[4] == car[2] & car[5] == "booked"):
-        car[5]="rented"
+    if(costumer['booked'] == car['reg'] and car['status'] == "booked"):
+        car['status']="rented"
 
-    return update_car(car[0], car[1], car[2], car[3], car[4], car[5])
+    return update_car(car['make'], car['model'], car['year'], car['reg'], car['location'], car['status'])
 
-@app.route('/return-car', methods=['GET']) 
-def rent_car(id, reg, status):
+@app.route('/return-car', methods=['POST']) 
+def return_car(id, reg, status):
     costumer = read_costumer(id)
     car = read_car(reg)
-    if(costumer[4] == car[2] & car[5] == "rented"):
-        update_costumer(costumer[0], costumer[1], costumer[2], costumer[3], "null")
+    if(costumer['booked'] == car['reg'] and car['status'] == "rented"):
+        update_costumer(costumer['name'], costumer['id'], costumer['age'], costumer['address'], "null")
         if(status == "ok"):
-            car[5] = "available"
+            car['status'] = "available"
         else:
-            car[5] = "damaged"
+            car['status'] = "damaged"
 
-    return update_car(car[0], car[1], car[2], car[3], car[4], car[5])
+    return update_car(car['make'], car['model'], car['year'], car['reg'], car['location'], car['status'])
 
 
 
